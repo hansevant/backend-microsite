a72f39b131dec7aec0772d9649ffb6c3a3777565
Hans Evan Tatipata <thansevan@gmail.com>
2023-11-11 15:33:27 +0700

    add new table

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv"
 import userRoutes from "./routes/users.js"
 import contactRoutes from "./routes/contact.js"
+import faqRoutes from "./routes/faq.js"
+import testiRoutes from "./routes/testi.js"
 dotenv.config()
 // import router from "./routes/routes.js";
 // import fileUpload from "express-fileupload";
@@ -22,6 +24,8 @@ app.get('/', async (req, res) => {
 
 app.use('/users', userRoutes)
 app.use('/contact', contactRoutes)
+app.use('/faq', faqRoutes)
+app.use('/testi', testiRoutes)
 
 app.use((req, res, next) => {
   next(error.NotFound());
